b196e43e74be55ab5a7082160365f8625f7a4eb9
prashanta0234 <prashanta0234@gmail.com>
2022-11-09 21:30:40 +0600

    all done

--- src/component/Todo/Todo.tsx
+++ src/component/Todo/Todo.tsx
@@ -11,31 +11,67 @@ import React from "react";
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import { todointer } from "../../interfaces/interface";
+import { todointer, TodoInterface } from "../../interfaces/interface";
 
 import "../Header/Header.css";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../rtk/app/Store";
-import { removeTodo } from "../../rtk/todo/todothunk";
+import { editTodo, removeTodo } from "../../rtk/todo/todothunk";
+
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DownloadDoneIcon from "@mui/icons-material/DownloadDone";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
 
 type todoitem = {
 	todo: todointer;
 };
+const Transition = React.forwardRef(function Transition(
+	props: TransitionProps & {
+		children: React.ReactElement<any, any>;
+	},
+	ref: React.Ref<unknown>
+) {
+	return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Todo: React.FC<todoitem> = props => {
 	const { todo } = props;
 	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 	const dispatch = useDispatch<AppDispatch>();
-	const open = Boolean(anchorEl);
+
+	const { register, handleSubmit, reset } = useForm<TodoInterface>();
+	const [open, setOpen] = React.useState(false);
+	const opens = Boolean(anchorEl);
+
 	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
 		setAnchorEl(event.currentTarget);
 	};
 	const handleClose = () => {
 		setAnchorEl(null);
 	};
+	const handleClickOpen = () => {
+		setOpen(true);
+		handleClose();
+	};
+	const handleCloseing = () => {
+		setOpen(false);
+		handleClose();
+	};
 
 	const handleDelete = () => {
 		dispatch(removeTodo(todo.id));
+		handleClose();
+	};
+
+	const onSubmit: SubmitHandler<TodoInterface> = data => {
+		console.log(data);
+		dispatch(editTodo({ id: todo.id, data: data }));
+		handleCloseing();
 	};
 
 	return (
@@ -56,9 +92,9 @@ const Todo: React.FC<todoitem> = props => {
 								</Typography>
 								<Button
 									className="btn-option"
-									aria-controls={open ? "basic-menu" : undefined}
+									aria-controls={opens ? "basic-menu" : undefined}
 									aria-haspopup="true"
-									aria-expanded={open ? "true" : undefined}
+									aria-expanded={opens ? "true" : undefined}
 									onClick={handleClick}
 								>
 									<DragIndicatorIcon />{" "}
@@ -66,13 +102,13 @@ const Todo: React.FC<todoitem> = props => {
 								<Menu
 									id="basic-menu"
 									anchorEl={anchorEl}
-									open={open}
+									open={opens}
 									onClose={handleClose}
 									MenuListProps={{
 										"aria-labelledby": "basic-button",
 									}}
 								>
-									<MenuItem onClick={handleClose} sx={{ color: "#616161" }}>
+									<MenuItem onClick={handleClickOpen} sx={{ color: "#616161" }}>
 										<EditIcon /> &nbsp; Update
 									</MenuItem>
 									<MenuItem onClick={handleDelete} sx={{ color: "#ff6e40" }}>
@@ -84,6 +120,47 @@ const Todo: React.FC<todoitem> = props => {
 							<Typography sx={{ color: "#616161" }}>{todo.body}</Typography>
 						</Box>
 					</Box>
+					<Dialog
+						open={open}
+						TransitionComponent={Transition}
+						keepMounted
+						onClose={handleClose}
+						aria-describedby="alert-dialog-slide-description"
+					>
+						<DialogTitle>{"Add your todo!"}</DialogTitle>
+						<DialogContent>
+							<Box
+								component="form"
+								justifyContent="center"
+								alignItems="center"
+								sx={{ display: "flex", py: "4px", flexDirection: "column" }}
+								onSubmit={handleSubmit(onSubmit)}
+							>
+								<input
+									className="input"
+									{...register("title")}
+									placeholder="Enter Todo Title"
+									required
+									defaultValue={todo.title}
+								/>
+								<textarea
+									{...register("body")}
+									className="input"
+									placeholder="Enter Todo Body"
+									required
+									defaultValue={todo.body}
+								/>
+
+								<Button variant="outlined" type="submit">
+									<DownloadDoneIcon />
+									&nbsp; ADD
+								</Button>
+							</Box>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={handleCloseing}>Cencel</Button>
+						</DialogActions>
+					</Dialog>
 				</Container>
 			</Box>
 		</>
